4b06ee029d295c59cba7092b7ef0ceaa2f525052
AXT940 <AXT940@student.bham.ac.uk>
2020-05-23 14:27:35 +0100

    Created blog app and Post model. New blog/ page with HTTP message.

--- hostingSite/settings.py
+++ hostingSite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*%0o7b&73%e%icmkz#x*e26!we74a9#3sl8jzccyy2pfv_bnp+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
