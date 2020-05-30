341de303d4ed19e4b28b6e41106019c400c2c913
AXT940 <AXT940@student.bham.ac.uk>
2020-05-30 15:19:56 +0100

    Created the post list, post detail and started new post form pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='post_index'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.new_post, name='new_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .forms import PostForm
 from django.http import HttpResponse
+
+from .models import Post
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Opening page of the blog, display the last posts.")
+    posts = Post.objects.all().order_by('published_date')
+    if not posts:
+        return HttpResponse("There is no posts do view for now.")
+    else :
+        return render(request, 'blog/post_index.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
+
+def new_post(request):
+    """if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_index.html', {'form': form})"""
+    return HttpResponse("This is the new post page")
