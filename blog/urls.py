99949a2df65f9e10821178cc8e41350b18ed5f5a
AXT940 <AXT940@student.bham.ac.uk>
2020-06-05 19:22:36 +0100

    Added the ability to edit posts

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='post_index'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.new_post, name='new_post'),
+    path('post/edit/<int:pk>/', views.post_edit, name='edit_post'),
 ]
